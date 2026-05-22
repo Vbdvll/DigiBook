c7b703095f32d3f5b9d402a12c051da6b31b84ac
abdallah konate <abdallah.konate@uadb.edu.sn>
2026-05-22 09:31:34 +0000

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
 window.DIGIBOOK_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://tsonaxeptfvualbxoiss.supabase.co",
+  anonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzb25heGVwdGZ2dWFsYnhvaXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0Mjg1NjEsImV4cCI6MjA5NTAwNDU2MX0.oi9uCmenatM-49vUpRu4S5iKuaO4A0GJSjfhQCFC-xw",
 };
